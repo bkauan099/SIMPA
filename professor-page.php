70f4c0db6b62e9536150bd4f1a1653cca7b12cf6
aianarnaud <aian.arnaudvs@gmail.com>
2026-03-30 13:48:55 -0300

    renomeando pagina

--- professor-page.php
+++ professor-page.php
@@ -23,7 +23,7 @@ if (!empty($_GET['ajax'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SIMPA PROFESSOR - UEMA</title>
+    <title>SIMPA - UEMA</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
